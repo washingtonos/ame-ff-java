49765f1b9f2e4b26ed1e63321b05fcf8bd4b55b5
Washington <washington.osouza@amedigital.com>
2022-09-18 19:08:11 -0300

    add estrutura do projeto

---
+++ src/main/java/com/fastforward/ameffjava/AmeFfJavaApplication.java
@@ -0,0 +1,13 @@
+package com.fastforward.ameffjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmeFfJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmeFfJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fastforward/ameffjava/AmeFfJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastforward.ameffjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmeFfJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
