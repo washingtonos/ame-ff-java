e93e023b0fe31bfc63558e35e1b0f6f47a1213e7
Washington <washington.osouza@amedigital.com>
2022-09-24 11:04:53 -0300

    add update e delete para o card utilizando jpa

--- src/main/java/com/fastforward/ameffjava/controller/CardController.java
+++ src/main/java/com/fastforward/ameffjava/controller/CardController.java
@@ -3,12 +3,15 @@ package com.fastforward.ameffjava.controller;
 import com.fastforward.ameffjava.controller.request.CardRequest;
 import com.fastforward.ameffjava.model.Card;
 import com.fastforward.ameffjava.service.CardService;
+import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,8 +36,27 @@ public class CardController {
     return cardService.findById(id);
   }
 
+  @GetMapping
+  public List<Card> listCards(){
+    LOG.info("Buscando todos as cartas");
+    return cardService.listAll();
+  }
+
   @PostMapping
   public  Card createCard(@RequestBody CardRequest cardRequest){
+    LOG.info("Criando carta: [{}]", cardRequest);
     return cardService.createCard(cardRequest);
+  }
+
+  @PutMapping("{id}")
+  public Card updateCard(@PathVariable("id") long id, @RequestBody CardRequest cardRequest){
+    LOG.info("Atualizando carta com id [{}]", id);
+    return cardService.update(id, cardRequest);
+  }
+
+  @DeleteMapping("{id}")
+  public void deleteCard(@PathVariable("id") long id){
+    LOG.info("Deletando carta com id [{}]", id);
+    cardService.delete(id);
   }
 }
--- src/main/java/com/fastforward/ameffjava/service/CardService.java
+++ src/main/java/com/fastforward/ameffjava/service/CardService.java
@@ -6,6 +6,7 @@ import com.fastforward.ameffjava.model.Card;
 import com.fastforward.ameffjava.repository.CardOriginRepository;
 import com.fastforward.ameffjava.repository.CardRepository;
 import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -24,6 +25,10 @@ public class CardService {
   public Card findById(long id){
     return this.cardRepository.findById(id)
         .orElseThrow(() -> new EntityNotFoundException("Card id [" + id + " ] not found."));
+  }
+
+  public List<Card> listAll(){
+    return this.cardRepository.findAll();
   }
 
   public Card createCard(CardRequest cardRequest){
@@ -48,4 +53,32 @@ public class CardService {
     return cardRepository.save(card);
   }
 
+  public Card update(long id, CardRequest cardRequest){
+    var card = this.findById(id);
+
+    card.setName(cardRequest.getName());
+    card.setDescription(cardRequest.getDescription());
+    card.setImageUrl(cardRequest.getImageUrl());
+    card.setStrength(cardRequest.getStrength());
+    card.setGear(cardRequest.getGear());
+    card.setSkill(cardRequest.getSkill());
+    card.setSpeed(cardRequest.getSpeed());
+    card.setIntellect(cardRequest.getIntellect());
+    card.setUpdateAt(LocalDateTime.now());
+
+    if(card.getOrigin() == null || card.getOrigin().getId() != cardRequest.getOriginId()){
+      var origin = cardOriginRepository.findById(cardRequest.getOriginId())
+          .orElseThrow(() -> new EntityNotFoundException("Card origin id [ "+ cardRequest.getOriginId() +" ] not found "));
+
+      card.setOrigin(origin);
+    }
+
+    return cardRepository.save(card);
+  }
+
+  public void delete(long id){
+    var card = this.findById(id);
+
+    cardRepository.delete(card);
+  }
 }
